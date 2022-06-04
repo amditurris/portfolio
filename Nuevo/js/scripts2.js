16c0be6efd54d4e066473584006d94825cbdd495
amditurris <105605491+amditurris@users.noreply.github.com>
2022-06-03 23:36:45 -0300

    Portfolio.1
    Version1

---
+++ Nuevo/js/scripts2.js
@@ -0,0 +1,27 @@
+document.getElementById("idheader").innerHTML = `
+        <ol>
+            <ul class="nav1">
+                <li><a href="../Nuevo/index.html" id="Name">A.M.D.T.</a></li>
+                <li><a href="../Nuevo/index.html" id="DW">Desarrolladora web</a></li>
+            </ul>
+
+            <ul class="nav">
+                <li class="img">
+                    <a href=""><img src="img/sp.png" width="40" height="40"></a>
+                    <a href=""><img src="img/br.png" width="45" height="45"></a>
+                    <a href=""><img src="img/uk.png" width="39" height="39"></a>
+                <li>   
+                <li class="primero"><a href="#sobremi">Sobre mi</a></li>
+                <li><a href="#habilidades">Habilidades</a></li>
+                <li><a href="pdf/CV-SP.pdf" target="_blank">CV</a></li>
+                <li><a>Trabajos</a></li>
+                <li><a href="#contacto">Contacto</a></li>   
+            </ul>
+
+            <ul class="redes">
+                <li><a href="http://www.linkedin.com/in/amditurris" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                <li><i class="fa fa-github"></i></li>
+                <li class="torta"><i class="fa-solid fa-bars"></i></li>
+            </ul>
+        </ol>
+`
